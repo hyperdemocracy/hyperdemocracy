23cbfde7ff7260efa0db0a8456509802e61cf1b1
Gabriel Altay <gabriel.altay@gmail.com>
2023-09-16 19:25:33 -0400

    ready for local runs

--- legisqa/legisqa/streamlit_app.py
+++ legisqa/legisqa/streamlit_app.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import json
+import os
 from pathlib import Path
 
 import chromadb
@@ -11,59 +12,42 @@ from langchain import HuggingFaceHub
 from langchain.llms import LlamaCpp
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
+import openai
 
 
 st.set_page_config(layout="wide")
 
-
-template = """Use the following pieces of context from congressional legislation to answer the question at the end.
-Remember that you can only answer questions about the content of the legislation. If you don't know the answer, just say that you don't know, don't try to make up an answer.
-{context}
-Question: {question}
-Helpful Answer:"""
-QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
-
-model_name = "BAAI/bge-small-en"
-model_kwargs = {'device': 'cpu'}
-encode_kwargs = {'normalize_embeddings': True}
-embedder = HuggingFaceBgeEmbeddings(
-    model_name=model_name,
-    model_kwargs=model_kwargs,
-    encode_kwargs=encode_kwargs
-)
-chroma_client = chromadb.PersistentClient(path="chroma.db")
-collection = chroma_client.get_collection(
-    name='uscb',
-    embedding_function=embedder.embed_documents,
-)
-vectorstore = Chroma(
-    collection_name="uscb",
-    embedding_function=embedder,
-    persist_directory="chroma.db",
-    client=chroma_client,
-)
+LLM_PROVIDERS = ["openai-chat", "hfhub", "llamacpp"]
+OPENAI_CHAT_MODELS = [
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-16k",
+    "gpt-4",
+    "gpt-4-32k",
+]
 
 
-st.title("LegisQA: Explore the Legislation of the 118th Congress")
-st.write(
-    """When you send a question to LegisQA, it will attempt to retrieve relevant content from the [118th United States Congress](https://en.wikipedia.org/wiki/118th_United_States_Congress), pass it to a [large language model (LLM)](https://en.wikipedia.org/wiki/Large_language_model), and generate an answer. This technique is known as Retrieval Augmented Generation (RAG) (see [here](https://research.ibm.com/blog/retrieval-augmented-generation-RAG) and [here](https://proceedings.neurips.cc/paper/2020/hash/6b493230205f780e1bc26945df7481e5-Abstract.html)). The retrieved content will be available for inspection with links to the bills and sponsors.
-This technique helps to ground the LLM response by providing context from a trusted source, but it does not guarantee a high quality answer. We encourage you to play around. Try different models. Find questions that work and find questions that fail."""
-)
 
-
-LLM_PROVIDERS_MODELS = {
-    "openai-chat": [
-        "gpt-3.5-turbo",
-        "gpt-3.5-turbo-16k",
-        "gpt-4",
-    ],
-    "hfhub": [
-        "databricks/dolly-v2-3b",
-        "google/flan-t5-large",
-        "google/flan-t5-small",
-    ],
-    "llamacpp": []
-}
+def load_vectorstore():
+    model_name = "BAAI/bge-small-en"
+    model_kwargs = {'device': 'cpu'}
+    encode_kwargs = {'normalize_embeddings': True}
+    embedder = HuggingFaceBgeEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+    )
+    chroma_client = chromadb.PersistentClient(path="chroma.db")
+    collection = chroma_client.get_collection(
+        name='uscb',
+        embedding_function=embedder.embed_documents,
+    )
+    vectorstore = Chroma(
+        collection_name="uscb",
+        embedding_function=embedder,
+        persist_directory="chroma.db",
+        client=chroma_client,
+    )
+    return vectorstore
 
 
 def get_sponsor_link(sponsors):
@@ -73,25 +57,80 @@ def get_sponsor_link(sponsors):
     return "[{}]({})".format(dd["fullName"], url)
 
 
+vectorstore = load_vectorstore()
+
+
+DEFAULT_PROMPT_TEMPLATE = """Use the following pieces of context from congressional legislation to answer the question at the end.
+Remember that you can only answer questions about the content of the legislation. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+{context}
+Question: {question}
+Helpful Answer:"""
+
+
+
+st.title(":classical_building: LegisQA :computer:")
+st.header("Explore the Legislation of the 118th US Congress")
+st.write(
+    """When you send a question to LegisQA, it will attempt to retrieve relevant content from the [118th United States Congress](https://en.wikipedia.org/wiki/118th_United_States_Congress), pass it to a [large language model (LLM)](https://en.wikipedia.org/wiki/Large_language_model), and generate an answer. This technique is known as Retrieval Augmented Generation (RAG). You can read the [original paper](https://proceedings.neurips.cc/paper/2020/hash/6b493230205f780e1bc26945df7481e5-Abstract.html) or a [recent summary](https://research.ibm.com/blog/retrieval-augmented-generation-RAG) to get more details. Once the answer is generated, the retrieved content will be available for inspection with links to the bills and sponsors.
+This technique helps to ground the LLM response by providing context from a trusted source, but it does not guarantee a high quality answer. We encourage you to play around. Try different models. Find questions that work and find questions that fail."""
+)
+
+
+
+
 with st.sidebar:
 
-    st.header("Learn about [hyperdemocracy](https://hyperdemocracy.us)")
+    st.subheader(":brain: Learn about [hyperdemocracy](https://hyperdemocracy.us)")
+    st.subheader(":world_map: Visualize with [nomic atlas](https://atlas.nomic.ai/map/b65c568b-ce37-40a1-b376-b20cc7580118/91b3337c-f6c9-4c1c-a755-18c84aa4141c)")
 
-    with st.expander("LLM choices"):
+    st.divider()
 
-        llm_provider = st.selectbox(
-            label="llm provider",
-            options=LLM_PROVIDERS_MODELS.keys(),
+    llm_provider = st.selectbox(
+        label="llm provider",
+        options=LLM_PROVIDERS,
+    )
+
+    if llm_provider == "openai-chat":
+        llm_name_options = OPENAI_CHAT_MODELS
+        llm_name = st.selectbox(
+            label="llm",
+            options=llm_name_options,
         )
+        openai_api_key = st.text_input(
+            "Provide your OpenAI API key here (sk-...)",
+            type="password",
+        )
+        if openai_api_key == "":
+            st.stop()
 
-        if llm_provider == "llamacpp":
-            gguf_path = Path(st.text_input("gguf_path"))
-            gguf_files = sorted(list(gguf_path.glob("*.gguf")))
-            gguf_map = {gf.name: gf for gf in gguf_files}
-            llm_name_options = gguf_map.keys()
+
+    elif llm_provider == "hfhub":
+        llm_name = st.text_input(
+            "Provide a HF model name (google/flan-t5-large)",
+        )
+        hfhub_api_token = st.text_input(
+            "Provide your HF API token here (hf_...)",
+            type="password",
+        )
+        if hfhub_api_token == "":
+            st.stop()
+
+
+    elif llm_provider == "llamacpp":
+        gguf_path = st.text_input("Provide a path to *.gguf files")
+        if gguf_path == "":
+            st.stop()
         else:
-            llm_name_options = LLM_PROVIDERS_MODELS[llm_provider]
-
+            gguf_path = Path(gguf_path)
+        if not gguf_path.exists():
+            st.warning("Provided gguf path does not exists")
+            st.stop()
+        gguf_files = sorted(list(gguf_path.glob("*.gguf")))
+        if len(gguf_files) == 0:
+            st.warning("Provided gguf path contains no gguf files")
+            st.stop()
+        gguf_map = {gf.name: gf for gf in gguf_files}
+        llm_name_options = gguf_map.keys()
         llm_name = st.selectbox(
             label="llm",
             options=llm_name_options,
@@ -112,7 +151,11 @@ with st.sidebar:
         top_p = st.slider('top_p', min_value=0.0, max_value=1.0, value=1.0)
 
         if llm_provider == "openai-chat":
-            llm = ChatOpenAI(model_name=llm_name, temperature=temperature)
+            llm = ChatOpenAI(
+                model_name=llm_name,
+                temperature=temperature,
+                openai_api_key=openai_api_key,
+            )
 
         elif llm_provider == "hfhub":
             max_length = st.slider(
@@ -123,7 +166,12 @@ with st.sidebar:
                 step=64
             )
             llm = HuggingFaceHub(
-                repo_id=llm_name, model_kwargs={"temperature": temperature, "max_length": max_length}
+                repo_id=llm_name,
+                huggingfacehub_api_token=hfhub_api_token,
+                model_kwargs={
+                    "temperature": temperature,
+                    "max_length": max_length,
+                }
             )
 
         elif llm_provider == "llamacpp":
@@ -149,9 +197,23 @@ with st.sidebar:
                 n_ctx=n_ctx,
             )
 
-        else:
-            st.error(f"{llm_name=} not recognized")
-            st.stop()
+
+    with st.expander("Prompt"):
+        prompt_template = st.text_area(
+            "prompt template",
+            DEFAULT_PROMPT_TEMPLATE,
+            height=300,
+        )
+
+
+qa_chain_prompt = PromptTemplate.from_template(prompt_template)
+qa_chain = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectorstore.as_retriever(search_kwargs={"k": n_ret_docs}),
+    chain_type_kwargs={"prompt": qa_chain_prompt},
+    return_source_documents=True,
+)
+
 
 col1, col2 = st.columns(2)
 
@@ -159,12 +221,6 @@ with col1:
 
     with st.form("my_form"):
         query = st.text_area('Enter question:')
-        qa_chain = RetrievalQA.from_chain_type(
-            llm,
-            retriever=vectorstore.as_retriever(search_kwargs={"k": n_ret_docs}),
-            chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
-            return_source_documents=True,
-        )
         submitted = st.form_submit_button('Submit')
 
 
@@ -176,52 +232,53 @@ def escape_markdown(text):
 
 if submitted:
     out = qa_chain({"query": query})
-    with col1:
-        st.info(escape_markdown(out["result"]))
-else:
-    st.stop()
+    st.session_state["out"] = out
 
+
+if not "out" in st.session_state:
+    st.stop()
+else:
+    out = st.session_state["out"]
+
+
+
+with col1:
+    st.info(escape_markdown(out["result"]))
 
 with col2:
 
-    if submitted:
+    # group source documents
+    grpd_source_docs = defaultdict(list)
+    for doc in out["source_documents"]:
+        grpd_source_docs[doc.metadata["parent_id"]].append(doc)
 
-        # group source documents
-        grpd_source_docs = defaultdict(list)
-        for doc in out["source_documents"]:
-            grpd_source_docs[doc.metadata["parent_id"]].append(doc)
-
-        # sort chunks in each group by start index
-        for key in grpd_source_docs:
-            grpd_source_docs[key] = sorted(
-                grpd_source_docs[key],
-                key=lambda x: x.metadata["start_index"],
-            )
-
-        # sort groups by number of chunks
-        grpd_source_docs = sorted(
-            tuple(grpd_source_docs.items()),
-            key=lambda x: -len(x[1]),
+    # sort chunks in each group by start index
+    for key in grpd_source_docs:
+        grpd_source_docs[key] = sorted(
+            grpd_source_docs[key],
+            key=lambda x: x.metadata["start_index"],
         )
 
-        for parent_id, doc_grp in grpd_source_docs:
-            first_doc = doc_grp[0]
-            ref = "{} chunks from {}\n\n[{}]({})\n\n{}".format(
-                len(doc_grp),
-                first_doc.metadata["parent_id"],
-                first_doc.metadata["title"],
-                first_doc.metadata["congress_gov_url"],
-                get_sponsor_link(first_doc.metadata["sponsors"]),
+    # sort groups by number of chunks
+    grpd_source_docs = sorted(
+        tuple(grpd_source_docs.items()),
+        key=lambda x: -len(x[1]),
+    )
+
+    for parent_id, doc_grp in grpd_source_docs:
+        first_doc = doc_grp[0]
+        ref = "{} chunks from {}\n\n[{}]({})\n\n{}".format(
+            len(doc_grp),
+            first_doc.metadata["parent_id"],
+            first_doc.metadata["title"],
+            first_doc.metadata["congress_gov_url"],
+            get_sponsor_link(first_doc.metadata["sponsors"]),
+        )
+        doc_contents = [
+            "[start_index={}] ".format(doc.metadata["start_index"]) + doc.page_content
+            for doc in doc_grp
+        ]
+        with st.expander(ref):
+            st.write(
+                escape_markdown("\n\n...\n\n".join(doc_contents))
             )
-            doc_contents = [
-                "[start_index={}] ".format(doc.metadata["start_index"]) + doc.page_content
-                for doc in doc_grp
-            ]
-            with st.expander(ref):
-                st.write(
-                    escape_markdown("\n\n...\n\n".join(doc_contents))
-                )
-
-
-
-
