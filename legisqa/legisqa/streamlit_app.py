b1715cc8ebdb8f606031d8f50a0b88bb8ef879d8
Gabriel Altay <gabriel.altay@gmail.com>
2024-02-24 14:36:55 -0500

    Update range (#29)
    * update db and filters
    
    * update text
    
    * update text

--- legisqa/legisqa/streamlit_app.py
+++ legisqa/legisqa/streamlit_app.py
@@ -6,11 +6,12 @@ from pathlib import Path
 
 import chromadb
 import streamlit as st
-from langchain.embeddings import HuggingFaceBgeEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chat_models import ChatOpenAI
-from langchain import HuggingFaceHub
-from langchain.llms import LlamaCpp
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_community.vectorstores import Chroma
+
+from langchain_openai import ChatOpenAI
+from langchain_community.llms import HuggingFaceHub
+from langchain_community.llms import LlamaCpp
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.chains.question_answering import load_qa_chain
@@ -20,76 +21,96 @@ import openai
 st.set_page_config(layout="wide", page_title="LegisQA")
 
 
-env_openai_api_key = os.getenv("OPENAI_API_KEY")
-env_hfhub_api_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")
-env_gguf_path = os.getenv("GGUF_PATH")
+env_openai_api_key = os.getenv("LEGISQA_OPENAI_API_KEY")
+env_hfhub_api_key = os.getenv("LEGISQA_HUGGINGFACEHUB_API_TOKEN")
+env_gguf_path = os.getenv("LEGISQA_GGUF_PATH")
+env_chroma_path = os.getenv("LEGISQA_CHROMA_PATH")
+
+
+CONGRESS_GOV_TYPE_MAP = {
+    "hconres": "house-concurrent-resolution",
+    "hjres": "house-joint-resolution",
+    "hr": "house-bill",
+    "hres": "house-resolution",
+    "s": "senate-bill",
+    "sconres": "senate-concurrent-resolution",
+    "sjres": "senate-joint-resolution",
+    "sres": "senate-resolution",
+}
 
 
 LLM_PROVIDERS = ["openai-chat", "hfhub", "llamacpp"]
 OPENAI_CHAT_MODELS = [
-    "gpt-3.5-turbo",
-    "gpt-3.5-turbo-16k",
-    "gpt-4",
-    "gpt-4-32k",
+    "gpt-3.5-turbo-0125",
+    "gpt-4-0125-preview",
 ]
 
 
-
 def load_vectorstore():
-    model_name = "BAAI/bge-small-en"
-    model_kwargs = {'device': 'cpu'}
-    encode_kwargs = {'normalize_embeddings': True}
-    embedder = HuggingFaceBgeEmbeddings(
+    model_name = "BAAI/bge-small-en-v1.5"
+    collection_name = "usc-113-to-118-vecs-v1-s1024-o256-BAAI-bge-small-en-v1.5"
+
+    chroma_client = chromadb.PersistentClient(path=env_chroma_path)
+    collection = chroma_client.get_collection(name=collection_name)
+    model_kwargs = {"device": "cpu"}
+    encode_kwargs = {"normalize_embeddings": True}
+    emb_fn = HuggingFaceBgeEmbeddings(
         model_name=model_name,
         model_kwargs=model_kwargs,
-        encode_kwargs=encode_kwargs
-    )
-    chroma_client = chromadb.PersistentClient(path="chroma.db")
-    collection = chroma_client.get_collection(
-        name='uscb',
-        embedding_function=embedder.embed_documents,
+        encode_kwargs=encode_kwargs,
+        query_instruction="Represent this question for searching relevant passages: ",
     )
     vectorstore = Chroma(
-        collection_name="uscb",
-        embedding_function=embedder,
-        persist_directory="chroma.db",
         client=chroma_client,
+        collection_name=collection_name,
+        embedding_function=emb_fn,
     )
     return vectorstore
 
 
-def get_sponsor_link(sponsors):
+def get_sponsor_link(anchor_text, bioguide_id):
     base_url = "https://bioguide.congress.gov/search/bio"
-    dd = json.loads(sponsors)[0]
-    url = "{}/{}".format(base_url, dd["bioguideId"])
-    return "[{}]({})".format(dd["fullName"], url)
+    url = "{}/{}".format(base_url, bioguide_id)
+    return "[{}]({})".format(anchor_text, url)
+
+
+def get_congress_gov_url(congress_num, legis_type, legis_num):
+    lt = CONGRESS_GOV_TYPE_MAP[legis_type]
+    return f"https://www.congress.gov/bill/{congress_num}th-congress/{lt}/{legis_num}"
+
+
+def get_govtrack_url(congress_num, legis_type, legis_num):
+    return (
+        f"https://www.govtrack.us/congress/bills/{congress_num}/{legis_type}{legis_num}"
+    )
 
 
 vectorstore = load_vectorstore()
 
 
-DEFAULT_PROMPT_TEMPLATE = """Use the following pieces of context from congressional legislation to answer the question at the end.
-Remember that you can only answer questions about the content of the legislation. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+DEFAULT_PROMPT_TEMPLATE = """Use the following pieces of context from congressional legislation to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
 {context}
+
 Question: {question}
 Helpful Answer:"""
 
 
-
 st.title(":classical_building: LegisQA :computer:")
-st.header("Explore the Legislation of the 118th US Congress")
+st.header("Explore Congressional Legislation")
 st.write(
-    """When you send a question to LegisQA, it will attempt to retrieve relevant content from the [118th United States Congress](https://en.wikipedia.org/wiki/118th_United_States_Congress), pass it to a [large language model (LLM)](https://en.wikipedia.org/wiki/Large_language_model), and generate an answer. This technique is known as Retrieval Augmented Generation (RAG). You can read the [original paper](https://proceedings.neurips.cc/paper/2020/hash/6b493230205f780e1bc26945df7481e5-Abstract.html) or a [recent summary](https://research.ibm.com/blog/retrieval-augmented-generation-RAG) to get more details. Once the answer is generated, the retrieved content will be available for inspection with links to the bills and sponsors.
+    """When you send a question to LegisQA, it will attempt to retrieve relevant content from the past six congresses ([113th-118th covering 2013 to the present](https://en.wikipedia.org/wiki/List_of_United_States_Congresses)), pass it to a [large language model (LLM)](https://en.wikipedia.org/wiki/Large_language_model), and generate an answer. This technique is known as Retrieval Augmented Generation (RAG). You can read the [original paper](https://proceedings.neurips.cc/paper/2020/hash/6b493230205f780e1bc26945df7481e5-Abstract.html) or a [recent summary](https://research.ibm.com/blog/retrieval-augmented-generation-RAG) to get more details. Once the answer is generated, the retrieved content will be available for inspection with links to the bills and sponsors.
 This technique helps to ground the LLM response by providing context from a trusted source, but it does not guarantee a high quality answer. We encourage you to play around. Try different models. Find questions that work and find questions that fail."""
 )
-
-
 
 
 with st.sidebar:
 
     st.subheader(":brain: Learn about [hyperdemocracy](https://hyperdemocracy.us)")
-    st.subheader(":world_map: Visualize with [nomic atlas](https://atlas.nomic.ai/map/b65c568b-ce37-40a1-b376-b20cc7580118/91b3337c-f6c9-4c1c-a755-18c84aa4141c)")
+    st.subheader(
+        ":world_map: Visualize with [nomic atlas](https://atlas.nomic.ai/data/gabrielhyperdemocracy/us-congressional-legislation-s1024o256nomic/map)"
+    )
+    st.subheader(":hugging_face: Explore the [huggingface datasets](https://huggingface.co/hyperdemocracy)")
 
     st.divider()
 
@@ -114,7 +135,6 @@ with st.sidebar:
         if openai_api_key == "":
             st.stop()
 
-
     elif llm_provider == "hfhub":
         llm_name = st.text_input(
             "Provide a HF model name (google/flan-t5-large)",
@@ -128,7 +148,6 @@ with st.sidebar:
             hfhub_api_token = env_hfhub_api_key
         if hfhub_api_token == "" or llm_name == "":
             st.stop()
-
 
     elif llm_provider == "llamacpp":
         if env_gguf_path is None:
@@ -156,7 +175,7 @@ with st.sidebar:
     with st.expander("Retrieval parameters"):
 
         n_ret_docs = st.slider(
-            'Number of chunks to retrieve',
+            "Number of chunks to retrieve",
             min_value=1,
             max_value=40,
             value=10,
@@ -164,8 +183,8 @@ with st.sidebar:
 
     with st.expander("Generative parameters"):
 
-        temperature = st.slider('temperature', min_value=0.0, max_value=2.0, value=0.0)
-        top_p = st.slider('top_p', min_value=0.0, max_value=1.0, value=1.0)
+        temperature = st.slider("temperature", min_value=0.0, max_value=2.0, value=0.0)
+        top_p = st.slider("top_p", min_value=0.0, max_value=1.0, value=1.0)
 
         if llm_provider == "openai-chat":
             llm = ChatOpenAI(
@@ -176,11 +195,7 @@ with st.sidebar:
 
         elif llm_provider == "hfhub":
             max_length = st.slider(
-                'max_tokens',
-                min_value=512,
-                max_value=16384,
-                value=512,
-                step=64
+                "max_tokens", min_value=512, max_value=16384, value=512, step=64
             )
             llm = HuggingFaceHub(
                 repo_id=llm_name,
@@ -188,23 +203,15 @@ with st.sidebar:
                 model_kwargs={
                     "temperature": temperature,
                     "max_length": max_length,
-                }
+                },
             )
 
         elif llm_provider == "llamacpp":
             n_ctx = st.slider(
-                'n_ctx',
-                min_value=512,
-                max_value=16384,
-                value=4096,
-                step=64
+                "n_ctx", min_value=512, max_value=16384, value=4096, step=64
             )
             max_tokens = st.slider(
-                'max_tokens',
-                min_value=512,
-                max_value=16384,
-                value=512,
-                step=64
+                "max_tokens", min_value=512, max_value=16384, value=512, step=64
             )
             llm = LlamaCpp(
                 model_path=str(gguf_map[llm_name]),
@@ -213,7 +220,6 @@ with st.sidebar:
                 top_p=top_p,
                 n_ctx=n_ctx,
             )
-
 
     with st.expander("Prompt"):
         prompt_template = st.text_area(
@@ -236,24 +242,30 @@ col1, col2 = st.columns(2)
 with col1:
 
     with st.form("my_form"):
-        query = st.text_area('Enter question:')
+        query = st.text_area("Enter question:")
         with st.expander("Filters"):
-            filter_bill_id = st.text_input("Bill ID (e.g. 118-S-2293)")
-#            filter_bio_id = st.text_input("Bio ID (e.g. R000595)")
-        submitted = st.form_submit_button('Submit')
+            filter_legis_id = st.text_input("Bill ID (e.g. 118-s-2293)")
+            filter_bioguide_id = st.text_input("Bioguide ID (e.g. R000595)")
+            filter_congress_num = st.text_input("Congress (e.g. 118)")
+        submitted = st.form_submit_button("Submit")
 
 
 def escape_markdown(text):
-    MD_SPECIAL_CHARS = "\`*_{}[]()#+-.!$"
+    MD_SPECIAL_CHARS = r"\`*_{}[]()#+-.!$"
     for char in MD_SPECIAL_CHARS:
-        text = text.replace(char, "\\"+char)
+        text = text.replace(char, "\\" + char)
     return text
+
 
 if submitted:
 
-    vs_filter = None
-    if filter_bill_id != "":
-        vs_filter = {"parent_id": filter_bill_id}
+    vs_filter = {}
+    if filter_legis_id != "":
+        vs_filter["legis_id"] = filter_legis_id
+    if filter_bioguide_id != "":
+        vs_filter["sponsor_bioguide_id"] = filter_bioguide_id
+    if filter_congress_num != "":
+        vs_filter["congress_num"] = int(filter_congress_num)
 
     rdocs_and_scores = vectorstore.similarity_search_with_score(
         query=query,
@@ -267,7 +279,7 @@ if submitted:
         st.warning("No documents were retrieved. Please check the filters.")
         st.stop()
 
-    out = qa_chain(
+    out = qa_chain.invoke(
         {
             "input_documents": rdocs,
             "question": query,
@@ -283,7 +295,6 @@ else:
     out = st.session_state["out"]
 
 
-
 with col1:
     do_escape_markdown = st.checkbox("escape markdown in answer")
     if do_escape_markdown:
@@ -296,7 +307,7 @@ with col2:
     # group source documents
     grpd_source_docs = defaultdict(list)
     for doc in out["input_documents"]:
-        grpd_source_docs[doc.metadata["parent_id"]].append(doc)
+        grpd_source_docs[doc.metadata["legis_id"]].append(doc)
 
     # sort chunks in each group by start index
     for key in grpd_source_docs:
@@ -311,24 +322,34 @@ with col2:
         key=lambda x: -len(x[1]),
     )
 
-    for parent_id, doc_grp in grpd_source_docs:
+    for legis_id, doc_grp in grpd_source_docs:
         first_doc = doc_grp[0]
         ref = "{} chunks from {}\n\n{}\n\n[congress.gov]({}) | [govtrack.us]({})\n\n{}".format(
             len(doc_grp),
-            first_doc.metadata["parent_id"],
+            first_doc.metadata["legis_id"],
             first_doc.metadata["title"],
-            first_doc.metadata["congress_gov_url"],
-            first_doc.metadata["govtrack_url"],
-            get_sponsor_link(first_doc.metadata["sponsors"]),
+            get_congress_gov_url(
+                first_doc.metadata["congress_num"],
+                first_doc.metadata["legis_type"],
+                first_doc.metadata["legis_num"],
+            ),
+            get_govtrack_url(
+                first_doc.metadata["congress_num"],
+                first_doc.metadata["legis_type"],
+                first_doc.metadata["legis_num"],
+            ),
+            get_sponsor_link(
+                first_doc.metadata["sponsor_full_name"],
+                first_doc.metadata["sponsor_bioguide_id"],
+            ),
         )
         doc_contents = [
             "[start_index={}] ".format(doc.metadata["start_index"]) + doc.page_content
             for doc in doc_grp
         ]
         with st.expander(ref):
-            st.write(
-                escape_markdown("\n\n...\n\n".join(doc_contents))
-            )
+            st.write(escape_markdown("\n\n...\n\n".join(doc_contents)))
+
 
 with st.sidebar:
 
@@ -343,7 +364,7 @@ with st.sidebar:
     }
     st.download_button(
         label="Download Results",
-        data = json.dumps(download_pack, indent=4),
-        file_name='legisqa_output.json',
-        mime='text/json',
+        data=json.dumps(download_pack, indent=4),
+        file_name="legisqa_output.json",
+        mime="text/json",
     )
